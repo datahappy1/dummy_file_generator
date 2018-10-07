68a837044eede7869d6a1597127062491bf4473b
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2018-10-07 21:12:44 +0200

    added classes

--- utils.py
+++ utils.py
@@ -14,18 +14,20 @@ def list_to_stmt(statement, add_plus_sign, add_comma):
         return str(statement).replace("'", "").strip('[]')
 
 
-def return_csv_header(column_list):
-    return list_to_stmt(statement=column_list, add_plus_sign=False, add_comma=False).replace(' ', '') + '\n'
+class Headers:
+    @staticmethod
+    def return_csv_header(column_list):
+        return list_to_stmt(statement=column_list, add_plus_sign=False, add_comma=False).replace(' ', '') + '\n'
 
+    @staticmethod
+    def return_flat_header(column_name_list, column_len_list):
+        header_row = []
 
-def return_flat_header(column_name_list, column_len_list):
-    header_row = []
+        for i, j in zip(column_name_list, column_len_list):
+            if len(i) > j:
+                logging.error(f'Header value for {i} is longer ({len(i)}) then '
+                              f'expected column length set in config.json file ({j})!')
+            else:
+                header_row.append(i + whitespace_generator(j - len(i)))
 
-    for i, j in zip(column_name_list, column_len_list):
-        if len(i) > j:
-            logging.error(f'Header value for {i} is longer ({len(i)}) then '
-                          f'expected column length set in config.json file ({j})!')
-        else:
-            header_row.append(i + whitespace_generator(j - len(i)))
-
-    return list_to_stmt(header_row, add_plus_sign=False, add_comma=False).replace(', ', '') + '\n'
+        return list_to_stmt(header_row, add_plus_sign=False, add_comma=False).replace(', ', '') + '\n'
