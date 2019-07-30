6d8539e8a846495fcef7d34dc74b7d56349b3dc0
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-07-31 00:16:46 +0200

    overall project refactoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dummy-file-generator",
-    version="0.0.1",
+    version="0.0.3",
     author="datahappy1",
     author_email="",
     description="A simplistic dummy flat text/csv file generator",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/datahappy1/dummy_file_generator",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
