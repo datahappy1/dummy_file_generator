e86fffbb2edd71716f7be9ee888d21e978e340d8
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-07-30 23:07:30 +0200

    overall project refactoring

---
+++ dummy_file_generator/__main__.py
@@ -0,0 +1,149 @@
+""" dummy file generator main runner """
+import argparse
+import json
+import os.path
+import io
+import logging
+
+from datetime import datetime
+from dummy_file_generator.lib.flat_writer import flat_row_header, flat_row_output
+from dummy_file_generator.lib.csv_writer import csv_row_header, csv_row_output
+from dummy_file_generator.configurables.settings import DEFAULT_ROW_COUNT, FILE_ENCODING, \
+    FILE_LINE_ENDING, CSV_VALUE_SEPARATOR
+
+
+def args():
+    """
+    argparse based argument parsing function
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pn', '--projectname', type=str, required=True)
+    parser.add_argument('-fn', '--filename', type=str, required=True)
+    parser.add_argument('-fs', '--filesize', type=int, required=False, default=0)
+    parser.add_argument('-rc', '--rowcount', type=int, required=False, default=0)
+    parser.add_argument('-gf', '--generated_files_location', type=str,
+                        required=False, default='generated_files')
+    parsed = parser.parse_args()
+
+    project_name = parsed.projectname
+    file_name = parsed.filename
+    file_size = parsed.filesize
+    row_count = parsed.rowcount
+    generated_files_location = parsed.generated_files_location
+    kwargs = {"project_name": project_name, "file_name": file_name, "file_size": file_size,
+              "row_count": row_count, "generated_files_location": generated_files_location}
+
+    obj = DummyFileGenerator(**kwargs)
+    DummyFileGenerator.main(obj)
+
+
+class DummyFileGenerator:
+    """
+    main project class
+    """
+    def __init__(self, **kwargs):
+        self.column_name_list = []
+        self.column_len_list = []
+        self.data_file_list = []
+        self.header = None
+        self.file_type = None
+        self.file_extension = None
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def read_config(self):
+        """
+        read config json file function
+        :return:
+        """
+        project_name = self.project_name  # pylint: disable=no-member
+        config_path = os.sep.join([os.path.join(os.path.dirname(__file__)), 'configurables'])
+
+        with open(config_path + os.sep + 'config.json') as file:
+            data = json.load(file)
+
+            for project in data['project']:
+                if project['project_name'] == project_name:
+                    self.header = project['header']
+                    self.file_type = project['file_type']
+                    self.file_extension = project['file_extension']
+                    for column in project['columns']:
+                        self.column_name_list.append(column['column_name'])
+                        self.data_file_list.append(str(column['datafile']).replace('.txt', ''))
+                        if self.file_type == "flat":
+                            self.column_len_list.append(column['column_len'])
+                    break
+            else:
+                raise ValueError("No such project found in config.json")
+
+    def write_output(self):
+        """
+        write output function
+        :return:
+        """
+        column_name_list = self.column_name_list
+        column_len_list = self.column_len_list
+        data_file_list = self.data_file_list
+        output_file_size = self.file_size * 1024  # pylint: disable=no-member
+        row_count = self.row_count  # pylint: disable=no-member
+        output_file_extension = '.' + self.file_extension
+        output_file_name = os.sep.join(['.', self.generated_files_location,  # pylint: disable=no-member
+                                        self.file_name + output_file_extension])  # pylint: disable=no-member
+
+        if row_count == 0 and output_file_size == 0:
+            # use default row_count from settings.py in case no row counts
+            # and no file size args provided:
+            row_count = DEFAULT_ROW_COUNT
+
+        with io.open(output_file_name, 'w', encoding=FILE_ENCODING) as output_file:
+            execution_start_time = datetime.now()
+            logging.info('File %s processing started at %s', output_file_name, execution_start_time)
+
+            if bool(self.header):
+                if self.file_type == "csv":
+                    output_file.write(csv_row_header(column_name_list, CSV_VALUE_SEPARATOR)
+                                      + FILE_LINE_ENDING)
+                elif self.file_type == "flat":
+                    output_file.write(flat_row_header(column_name_list, column_len_list)
+                                      + FILE_LINE_ENDING)
+
+            iterator = 1
+            while output_file.tell() < output_file_size or iterator < row_count:
+                if self.file_type == "csv":
+                    row = csv_row_output(data_file_list, CSV_VALUE_SEPARATOR)
+                elif self.file_type == "flat":
+                    row = flat_row_output(data_file_list, column_len_list)
+                output_file.write(row + FILE_LINE_ENDING)
+                iterator += 1
+
+                if divmod(iterator, 10000)[1] == 1:
+                    logging.info('%s rows written', iterator-1)
+
+            # to get the file_size even when only row_count arg used
+            output_file_size = output_file.tell()
+            output_file.close()
+
+            execution_end_time = datetime.now()
+            duration = (execution_end_time - execution_start_time).seconds
+            duration = str(duration/60) + ' min.' if duration > 1000 else str(duration) + ' sec.'
+
+            logging.info('File %s processing finished at %s\n '
+                         '%s kB file with %s rows written in %s',
+                         output_file_name, execution_end_time,
+                         output_file_size / 1024, iterator, duration)
+
+    def main(self):
+        """
+        main function
+        :return:
+        """
+        # set logging levels for main function console output
+        logging.getLogger().setLevel(logging.INFO)
+
+        DummyFileGenerator.read_config(self)
+        DummyFileGenerator.write_output(self)
+
+
+if __name__ == "__main__":
+    args()
